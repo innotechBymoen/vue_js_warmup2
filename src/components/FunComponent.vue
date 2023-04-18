dc457baa66aa2be7599966ea7f15ea6f2d4abe94
bymoen <alexb@innotechcollege.com>
2023-04-18 16:56:28 -0600

    Made the button and text clickable

---
+++ src/components/FunComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button @click="change_color">CLICK ME!</button>
+    <section>
+      <p v-for="(secret, i) in secrets" :key="i" @click="make_text_pink">
+        {{ secret }}
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      secrets: [
+        `Did you know that Vue was made by Evan Yue?`,
+        `Did you know that Vue is one of the most liked frameworks as voted by developers`,
+      ],
+    };
+  },
+
+  methods: {
+    change_color: function () {
+      document.body[`style`][`backgroundColor`] = `gold`;
+    },
+    make_text_pink(details) {
+      details[`target`][`style`][`color`] = `pink`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
